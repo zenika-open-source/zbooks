6c7e6a91187f24d7bc0309c2b027cc430bc26dcf
Antoine Duchenet <antoine.duchenet@zenika.com>
2024-03-01 16:36:05 +0100

    feat: 📜 display a book titles list

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
 use leptos::*;
 
 fn main() {
-    mount_to_body(|| view! { <p>"Hello, world!"</p> })
+    mount_to_body(BookList)
+}
+
+#[component]
+fn BookList() -> impl IntoView {
+    let books = vec![
+        "Software craft : TDD, Clean Code et autres pratiques essentielles",
+        "Designing Data-Intensive Applications: The Big Ideas Behind Reliable, Scalable, and Maintainable Systems",
+        "Accelerate: The Science of Lean Software and DevOps: Building and Scaling High Performing Technology"
+    ];
+
+    view! {
+        <ul class="p-8 list-disc list-inside">
+            {books.into_iter()
+                .map(|book| view! { <li class="my-2">{book}</li>})
+                .collect::<Vec<_>>()}
+        </ul>
+    }
 }
