43c35d3273814839aa2338100b6295e2dd73a594
Pierre-Yves Aillet <pyaillet@gmail.com>
2024-03-15 16:25:04 +0100

    :alembic: testing the addition of bookcards

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,65 @@ fn main() {
     mount_to_body(BookList)
 }
 
+enum Language {
+    EN,
+    FR,
+}
+
+struct Book {
+    title: String,
+    authors: Vec<String>,
+    language: Language,
+}
+
 #[component]
 fn BookList() -> impl IntoView {
     let books = vec![
-        "Software craft : TDD, Clean Code et autres pratiques essentielles",
-        "Designing Data-Intensive Applications: The Big Ideas Behind Reliable, Scalable, and Maintainable Systems",
-        "Accelerate: The Science of Lean Software and DevOps: Building and Scaling High Performing Technology"
+        Book {
+            title: "Software craft : TDD, Clean Code et autres pratiques essentielles".into(),
+            authors: vec![
+                "Cyrille Martraire".into(),
+                "Arnaud Thiéfaine".into(),
+                "Dorra Bartaguiz".into(),
+                "Fabien Hiegel".into(),
+                "Houssam Fakih".into(),
+            ],
+            language: Language::FR,
+        },
+        Book {
+            title: "Designing Data-Intensive Applications: The Big Ideas Behind Reliable, Scalable, and Maintainable Systems".into(),
+            authors: vec![
+                "Martin Kleppmann".into(),
+            ],
+            language: Language::EN,
+        },
+        Book {
+            title: "Accelerate: The Science of Lean Software and DevOps: Building and Scaling High Performing Technology".into(),
+            authors: vec![
+                "Nicole Forsgren".into(),
+                "Jez Humble".into(),
+                "Gene Kim".into(),
+            ],
+            language: Language::EN,
+        }
     ];
 
     view! {
         <ul class="p-8 list-disc list-inside">
             {books.into_iter()
-                .map(|book| view! { <li class="my-2">{book}</li>})
+                .map(|book| view! { <li class="my-2"><BookCard book=book /></li>})
+                .collect::<Vec<_>>()}
+        </ul>
+    }
+}
+
+#[component]
+fn BookCard(book: Book) -> impl IntoView {
+    view! {
+        <span>{book.title}</span>
+        <ul class="p-6 list-disc list-inside">
+            {book.authors.into_iter()
+                .map(|author| view! { <li class="my-2">{author}</li>})
                 .collect::<Vec<_>>()}
         </ul>
     }
